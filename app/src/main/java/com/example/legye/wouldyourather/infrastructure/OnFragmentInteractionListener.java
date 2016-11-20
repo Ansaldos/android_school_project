85b3aff4e250835376adbd3ab4996b89066a0136
Balogh <balogh.attila@ibcontroll.hu>
2016-11-20 12:42:11 +0100

    Deleted old files

--- app/src/main/java/com/example/legye/wouldyourather/infrastructure/OnFragmentInteractionListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.legye.wouldyourather.infrastructure;
-
-/**
- * Created by legye on 2016. 11. 20..
- */
-
-public interface OnFragmentInteractionListener {
-    void onFragmentMessage(String fragmentId, Object data);
-}
