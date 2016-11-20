61017ac1b38bd6b5c639efa1176bd7573e129e72
Balogh <balogh.attila@ibcontroll.hu>
2016-11-20 12:41:06 +0100

    Project rebased, moved to base folder

---
+++ WouldYouRather/app/src/main/java/com/example/legye/wouldyourather/infrastructure/OnFragmentInteractionListener.java
@@ -0,0 +1,9 @@
+package com.example.legye.wouldyourather.infrastructure;
+
+/**
+ * Created by legye on 2016. 11. 20..
+ */
+
+public interface OnFragmentInteractionListener {
+    void onFragmentMessage(String fragmentId, Object data);
+}
